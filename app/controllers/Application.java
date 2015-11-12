69c55160b63cebb97941ba477beb1da175997afb
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-11-12 15:14:56 -0500

    added new ballot page and new profile, no styling

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,9 +2,7 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
-import views.html.profile;
-import views.html.ballot;
+import views.html.*;
 import models.User;
 
 public class Application extends Controller {
@@ -19,5 +17,13 @@ public class Application extends Controller {
 
     public Result ballot() {
         return ok(ballot.render("No ballots to view right now"));
+    }
+
+    public Result newballot() {
+        return ok(newballot.render("No ballots to view right now"));
+    }
+
+    public Result newprofile() {
+        return ok(newprofile.render("No ballots to view right now"));
     }
 }
