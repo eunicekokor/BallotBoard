6c2ac98bd7e7007dbaa8e2ac86451a83c3ec9fdf
Steve Cheruiyot <s.cheruiyot@columbia.edu>
2015-11-13 03:54:13 -0500

    implementing ballot listing

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,17 @@
 package controllers;
 
+import models.Ballot;
+import models.User;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.data.*;
 import static play.data.Form.*;
-import views.html.index;
-import views.html.profile;
-import views.html.ballot;
-import views.html.login;
-import views.html.signup;
-import models.User;
+
+import play.mvc.Security;
+import views.html.*;
 
 public class Application extends Controller {
+
     /**
      * Handles requests to /login route
      * @return login form
@@ -21,10 +21,22 @@ public class Application extends Controller {
     }
 
     /**
+     * Logs out current user
+     * @return
+     */
+    public Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(
+                routes.Application.login()
+        );
+    }
+    /**
      * Authenticates a user
      * @return
      */
     public Result authenticate() {
+
         Form<Login> loginForm = form(Login.class).bindFromRequest();
 
         if (loginForm.hasErrors()) {
@@ -32,6 +44,24 @@ public class Application extends Controller {
         } else {
             session().clear();
             session("email", loginForm.get().email);
+            Ballot tmp = new Ballot();
+
+            //FIXME Adding test ballots
+            tmp.create("Kony 2012", "This vote will determine the fate of the universe.", "Nkrumah");
+            tmp.insert();
+
+            Ballot tmp1 = new Ballot();
+            tmp1.create("Kony 2013", "This vote will determine the fate of the universe.", "Azikiwe");
+            tmp1.insert();
+
+            Ballot tmp2 = new Ballot();
+            tmp2.create("Kony 2014", "This vote will determine the fate of the universe.", "Lumumba");
+            tmp2.insert();
+
+            Ballot tmp3 = new Ballot();
+            tmp3.create("Kony 2015", "This vote will determine the fate of the universe.", "Mandela");
+            tmp3.insert();
+
             return redirect(
                     routes.Application.ballot()
             );
@@ -43,7 +73,21 @@ public class Application extends Controller {
      * @return
      */
     public Result register() {
-        return ok();
+        Form<SignUp> signUpForm = form(SignUp.class).bindFromRequest();
+
+        if (signUpForm.hasErrors()) {
+            return badRequest(signup.render(signUpForm));
+        } else {
+            User newUser = new User();
+            newUser.create(signUpForm.get().username, signUpForm.get().fullname,
+                    signUpForm.get().email, signUpForm.get().password);
+            newUser.insert();
+            session().clear();
+            session("username", signUpForm.get().username);
+            return redirect(
+                    routes.Application.ballot()
+            );
+        }
     }
 
     /**
@@ -59,24 +103,49 @@ public class Application extends Controller {
      * @return
      */
     public Result signup() {
-        return ok(signup.render(""));
+        return ok(signup.render(form(SignUp.class)));
     }
 
-
     /**
-     * Handles /user route
+     * Create a new ballot
      * @return
      */
-    public Result user () {
-        return ok(profile.render("Welcome to your profile"));
+    @Security.Authenticated(Secured.class)
+    public Result create() {
+        Form<BallotForm> bForm = form(BallotForm.class).bindFromRequest();
+
+        if (bForm.hasErrors()) {
+            return badRequest(ballotForm.render(bForm));
+        } else {
+            Ballot tmp = new Ballot();
+            tmp.create(bForm.get().ballotName, bForm.get().description, request().username());
+            tmp.insert();
+            return redirect(
+                    routes.Application.user()
+            );
+        }
     }
 
     /**
      *
      * @return
      */
+    @Security.Authenticated(Secured.class)
+    public Result ballotForm() {
+        return ok(ballotForm.render(form(BallotForm.class)));
+    }
+
+    /**
+     * Handles /user route
+     * @return
+     */
+    @Security.Authenticated(Secured.class)
+    public Result user() {
+        return ok(profile.render(Ballot.findAll(), Ballot.findAll(), request().username()));
+    }
+
     public Result ballot() {
-        return ok(ballot.render("No ballots to view right now"));
+        return ok(ballot.render(Ballot.findAll(), request().username()));
     }
 
     /**
@@ -86,18 +155,54 @@ public class Application extends Controller {
 
         public String email;
         public String password;
-        //public static User loginUser;
         /**
          * Validates user's input on sign in
          * @return
          */
         public String validate() {
-            //loginUser = new User();
-            if (User.authenticate(email, password) == null) {
-                return "Invalid Email and/or Password";
+            if (User.authenticate(email, password)) {
+                return null;
+            }
+            return "Invalid Email and/or Password";
+        }
+
+    }
+
+    /**
+     * Form to store information entered by a new user
+     */
+    public static class SignUp {
+
+        public String email;
+        public String fullname;
+        public String username;
+        public String password;
+
+        /**
+         * Checks if an email or username is already taken
+         * @return
+         */
+        public String validate() {
+            if (User.exists(email, username)) {
+                return "Email and/or Username already taken";
             }
             return null;
         }
+    }
 
+    /**
+     * Form for a creating a new Ballot
+     */
+    public static class BallotForm {
+
+        public String ballotName;
+        public String description;
+
+        public String validate() {
+            if (Ballot.exists(ballotName, description)) {
+                return "It appears you are trying to create a ballot that already exists!";
+            }
+            return null;
+        }
     }
 }
--- app/models/User.java
+++ app/models/User.java
@@ -18,20 +18,69 @@ public class User {
     public String email;
     public String password;
 
+    /**
+     * Creates a new user
+     * @param username
+     * @param fullname
+     * @param email
+     * @param password
+     */
+    public void create(String username, String fullname, String email, String password) {
+        this.username = username;
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+    }
+
+    /**
+     * Insert this user in to db collection
+     */
     public void insert() {
         users().save(this);
     }
 
+    /**
+     * Remove this user from db collection
+     */
     public void remove() {
         users().remove(this.id);
     }
 
+    /**
+     * Finds user by username
+     * @param name,username of user
+     * @return matched User if any
+     */
     public static User findByUsername(String name) {
         return users().findOne("{username: #}", name).as(User.class);
     }
 
-
+    /**
+     * Checks if user as entered a valid email and password
+     * @param email
+     * @param password
+     * @return true if correct email and password and false otherwise
+     */
     public static Boolean authenticate(String email, String password) {
-        return null;
+        if (users().findOne("{ $and: [{email: #},{password: #}]}", email, password).as(User.class) == null) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if an email or username if already taken
+     * @param email
+     * @param username
+     * @return true if valid, false otherwise
+     */
+    public static Boolean exists(String email, String username) {
+        if (users().findOne("{email: #}", email).as(User.class) == null) {
+            return false;
+        }
+        if (users().findOne("{username: #}", username).as(User.class) == null) {
+            return false;
+        }
+        return true;
     }
 }
